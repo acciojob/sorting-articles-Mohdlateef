2c19cf8edfb413e2f7084382743956f4b3ccf344
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-22 20:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let root=document.getElementById("root");
+// root.innerText="abc"
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+let arr=bands.sort();
+arr.map((ele)=>{
+	let li=document.createElement("li")
+	li.innerText=ele;
+	root.append(li);
+	
+})
+
